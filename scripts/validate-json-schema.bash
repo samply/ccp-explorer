4b848ede4383b899ba3ee433051f78c17a5edb7d
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2025-03-28 14:11:20 +0100

    chore: remove old catalogue

--- scripts/validate-json-schema.bash
+++ scripts/validate-json-schema.bash
@@ -1,4 +1,3 @@
 set -e # Return non-zero exit status if one of the validations fails
 npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/catalogue.schema.json -d static/catalogues/catalogue-dktk.json
 npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/catalogue.schema.json -d static/catalogues/catalogue-dktk-staging.json
-npx ajv validate -c ajv-formats -s node_modules/@samply/lens/schema/catalogue.schema.json -d static/catalogues/catalogue-dktk-with-mol-markers.json
